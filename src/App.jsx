f7991f81b6ddc51551b397e9c523283285f6e9fd
Nick Bartlett <nickbartlett19@gmail.com>
2024-02-26 14:53:12 -0500

    update App() with CalendarCell components

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,154 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {CalendarCell} from "./CalendarCell"
+import "./styles.css"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1 className = "title">March</h1>
+      <div className = "grid">
+        <div className = "column">
+          <div className = "calendar-cell">
+            <h1 className = "card-header">1</h1>
+            <CalendarCell />
+          </div>
+          <div className = "calendar-cell">
+            <h1 className = "card-header">9</h1>
+            <CalendarCell />
+          </div>
+          <div className = "calendar-cell">
+            <h1 className = "card-header">17</h1>
+            <CalendarCell />
+          </div>
+          <div className = "calendar-cell">
+            <h1 className = "card-header">25</h1>
+            <CalendarCell />
+          </div>
+        </div>
+        <div className = "column">
+          <div className = "calendar-cell">
+            <h1 className = "card-header">2</h1>
+            <CalendarCell />
+          </div>
+          <div className = "calendar-cell">
+            <h1 className = "card-header">10</h1>
+            <CalendarCell />
+          </div>
+          <div className = "calendar-cell">
+            <h1 className = "card-header">18</h1>
+            <CalendarCell />
+          </div>
+          <div className = "calendar-cell">
+            <h1 className = "card-header">26</h1>
+            <CalendarCell />
+          </div>
+        </div>
+        <div className = "column">
+          <div className = "calendar-cell">
+            <h1 className = "card-header">3</h1>
+            <CalendarCell />
+          </div>
+          <div className = "calendar-cell">
+            <h1 className = "card-header">11</h1>
+            <CalendarCell />
+          </div>
+          <div className = "calendar-cell">
+            <h1 className = "card-header">19</h1>
+            <CalendarCell />
+          </div>
+          <div className = "calendar-cell">
+            <h1 className = "card-header">27</h1>
+            <CalendarCell />
+          </div>
+        </div>
+        <div className = "column">
+          <div className = "calendar-cell">
+            <h1 className = "card-header">4</h1>
+            <CalendarCell />
+          </div>
+          <div className = "calendar-cell">
+            <h1 className = "card-header">12</h1>
+            <CalendarCell />
+          </div>
+          <div className = "calendar-cell">
+            <h1 className = "card-header">20</h1>
+            <CalendarCell />
+          </div>
+          <div className = "calendar-cell">
+            <h1 className = "card-header">28</h1>
+            <CalendarCell />
+          </div>
+        </div>
+        <div className = "column">
+          <div className = "calendar-cell">
+            <h1 className = "card-header">5</h1>
+            <CalendarCell />
+          </div>
+          <div className = "calendar-cell">
+            <h1 className = "card-header">13</h1>
+            <CalendarCell />
+          </div>
+          <div className = "calendar-cell">
+            <h1 className = "card-header">21</h1>
+            <CalendarCell />
+          </div>
+          <div className = "calendar-cell">
+            <h1 className = "card-header">29</h1>
+            <CalendarCell />
+          </div>
+        </div>
+        <div className = "column">
+          <div className = "calendar-cell">
+            <h1 className = "card-header">6</h1>
+            <CalendarCell />
+          </div>
+          <div className = "calendar-cell">
+            <h1 className = "card-header">14</h1>
+            <CalendarCell />
+          </div>
+          <div className = "calendar-cell">
+            <h1 className = "card-header">22</h1>
+            <CalendarCell />
+          </div>
+          <div className = "calendar-cell">
+            <h1 className = "card-header">30</h1>
+            <CalendarCell />
+          </div>
+        </div>
+        <div className = "column">
+          <div className = "calendar-cell">
+            <h1 className = "card-header">7</h1>
+            <CalendarCell />
+          </div>
+          <div className = "calendar-cell">
+            <h1 className = "card-header">15</h1>
+            <CalendarCell />
+          </div>
+          <div className = "calendar-cell">
+            <h1 className = "card-header">23</h1>
+            <CalendarCell />
+          </div>
+          <div className = "calendar-cell">
+            <h1 className = "card-header">31</h1>
+            <CalendarCell />
+          </div>
+        </div>
+        <div className = "column">
+          <div className = "calendar-cell">
+            <h1 className = "card-header">8</h1>
+            <CalendarCell />
+          </div>
+          <div className = "calendar-cell">
+            <h1 className = "card-header">16</h1>
+            <CalendarCell />
+          </div>
+          <div className = "calendar-cell">
+            <h1 className = "card-header">24</h1>
+            <CalendarCell />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
